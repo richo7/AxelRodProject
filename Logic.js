933119680893bae5f8e83308e1d4a7672650f61f
Plihb <xuanyuanhan@gmail.com>
2017-03-28 22:10:04 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,39 +1,59 @@
+/* Maximum number of rounds before the game ends */
 const MAX_TURNS = 10;
+/* Rounds completed so far. Used to reset the game when required */
 let turnsDone = 0;
 
-let player1 = new Player()
-let player2 = new Player()
+/* The two player objects */
+let localPlayer = new Player()
+let foreignPlayer = new Player()
 
+/* The player class */
 class Player {
-    years: number;
-    inputted: boolean;
-    betray: boolean;
+    years: number;     
+    inputted: boolean; //have they inputted for the current round ?
+    betray: boolean;   //their choice of action for the current
 
     constructor() {
         this.years = 0;
     }
 }
 
-radio.onDataPacketReceived((receivedNumber) => {
-    let n = receivedNumber.receivedString;
+radio.onDataPacketReceived(({receivedNumber}) => {
+    /* The choice of the foreign player */
+    if (foreignPlayer.inputted)
+        return;
+    
+    foreignPlayer.inputted = true;
+    let n = receivedNumber;
+    foreignPlayer.betray = n == 1;
 
-    if (n === "0" || n === "1") {
-        if (player1.inputted)
-            return;
+    tryToProceed();
+})
+radio.setGroup(1);
 
-        player1.inputted = true;
-        player1.betray = n === "1";
-    } else if (n === "2" || n === "3") {
-        if (player2.inputted)
-            return;
+//stay silent
+input.onButtonPressed(Button.A, () => {
+    if(localPlayer.inputted)
+        return;
+    
+    localPlayer.inputted = true;
+    localPlayer.betray = false;
+})
 
-        player2.inputted = true;
-        player2.betray = n === "3";
-    }
+//betray
+input.onButtonPressed(Button.B, () => {
+    if (localPlayer.inputted)
+        return;
 
-    if (player1.inputted && player2.inputted) {
-        player1.inputted = false;
-        player2.inputted = false;
+    localPlayer.inputted = true;
+    localPlayer.betray = true;    
+})
+
+function tryToProceed() {
+
+    if (localPlayer.inputted && foreignPlayer.inputted) {
+        localPlayer.inputted = false;
+        foreignPlayer.inputted = false;
 
         resolve();
 
@@ -43,34 +63,30 @@ radio.onDataPacketReceived((receivedNumber) => {
     }
 
     if (turnsDone >= MAX_TURNS) {
-
-        //GAME END
-
         reset();
     }
-})
-radio.setGroup(1);
+}
 
 function resolve() {
-    let input1 = player1.betray;
-    let input2 = player2.betray;
+    let input1 = localPlayer.betray;
+    let input2 = foreignPlayer.betray;
 
     if (input1 && input2) { //both betray
-        player1.years += 2;
-        player2.years += 2;
+        localPlayer.years += 2;
+        foreignPlayer.years += 2;
     } else if (!input1 && !input2) { //both silent
-        player1.years += 1;
-        player2.years += 1;
+        localPlayer.years += 1;
+        foreignPlayer.years += 1;
     } else { //1 betray, 1 silent
         if (input1) { //player 1 betray, player 2 silent
-            player2.years += 3;
+            foreignPlayer.years += 3;
         } else if (input2) { //player 2 betray, player 1 silent
-            player1.years += 3;
+            localPlayer.years += 3;
         }
     }
 }
 
 function reset() {
-    player1.years = 0;
-    player2.years = 0;
+    localPlayer.years = 0;
+    foreignPlayer.years = 0;
 }
